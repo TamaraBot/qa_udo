68763b133bd3a3c7c7e7d557c76cf16f308e12f8
Tamara Botvinkina <toma.ne@sibmail.com>
2022-01-15 23:35:47 +0700

    test

--- src/test/java/online_tusur/unit_online_tusur/StudentTest.java
+++ src/test/java/online_tusur/unit_online_tusur/StudentTest.java
@@ -1,5 +1,7 @@
 package online_tusur.unit_online_tusur;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 
@@ -8,16 +10,77 @@ public class StudentTest {
 	private Student s = new Student();	 	   
 
 	    
-/* проверка возраста студента, 
- выходящего за диапазон */
+//проверка возраста студента, входящий в диапазон
 	  @Test
-	  void test_assert() {
-		int age = 34;
+	  @Tag("positive")
+	  void testAge_1() {
+		int age = 30;
 		s.setAge(age);
-	    int expectedAge = 34;
+	    int expectedAge = 30;
 	    int actualsAge=s.getAge();
 	    assertEquals(expectedAge, actualsAge);	
 
 	  }
-	  
+	 
+	 //позитивный тест на возраст (граничное значение 50)
+	 @Test
+	  @Tag("positive")
+	  void testAge_2() {
+		int age = 50;
+		s.setAge(age);
+	    int expectedAge = 50;
+	    int actualsAge=s.getAge();
+	    assertEquals(expectedAge, actualsAge);	
+	  }
+	 
+	 //позитивный тест на возраст (граничное значение 18)
+	 @Test
+	  @Tag("positive")
+	  void testAge_3() {
+		int age = 18;
+		s.setAge(age);
+	    int expectedAge = 18;
+	    int actualsAge=s.getAge();
+	    assertEquals(expectedAge, actualsAge);	
+	  }
+	
+	 
+	 //негативный тест на написание фамилии с маленькой буквы
+	 @Test
+	 @Tag("negative")
+	 void testLastNameInvalid() {
+		 String fio = "petrov";
+		 s.setLastName(fio);
+		 String expectedName= "Petrov";
+		 String actualsName = s.getLastName();
+		 assertEquals(expectedName, actualsName); 	 
+	 }
+
+	  //негативный тест на возрат студента, выходящего за диапазон
+	 @Test
+	 @Tag("negative")
+	 void testAgeInvalid() {
+		 int age = - 17;
+		 s.setAge(age);
+		 int expectedAge=18;
+		 int actualAge = s.getAge();
+		 assertEquals(expectedAge, actualAge); 	 
+	 }
+	 
+	 
+	 //негативный тест на ввод имени всеми прописными буквами
+	 @Test
+	 @Tag("negative")
+	 void testNameInvalid() {
+		 String name = "IVAN";
+		 s.setFirstName(name);
+		 String expectedName= "Ivan";
+		 String actualsName = s.getFirstName();
+		 assertEquals(expectedName, actualsName); 	 
+	 }
+
+	 
+	 
+	 
+
 }
