eba0d3e1fc34adfabaf267d493f76462bdfbe247
Tamara Botvinkina <toma.ne@sibmail.com>
2022-02-03 21:48:18 +0700

    Test1

---
+++ src/test/java/online_tusur/unit_online_tusur/Test_NG.java
@@ -0,0 +1,60 @@
+package online_tusur.unit_online_tusur;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Test_NG {
+
+	private Student s = new Student();
+	
+
+	private ArrayList<Student> emp;
+
+		@BeforeTest
+		private void setUp() {
+			emp = new ArrayList<Student>();
+		}
+	
+		@AfterTest
+		private void tearDown() {
+			emp.clear();
+		}
+	
+	//Тест для сравнения заданных и полученных значений возраста, а также сравнения среднего возраста с нулем
+	@Test
+	@Parameters({"a", "d"})
+
+		void TestNG_SetAvg(@Optional("50") int a, @Optional("18") int d) {
+			int param =  a;
+			s.setAge(param);
+			assertEquals(s.getAge(),d);
+
+		}
+	@DataProvider (name = "data")
+	public static Object[][] getAge() {
+		return new Object[][]{{0,18}, {18,18}, {50,50}, {25,25}};
+	}
+	
+	@SuppressWarnings("static-access")
+	@Test (dataProvider = "data")
+	void TestAge(int input, int expected) {
+		s.setAge(input);
+		int actualAge = s.getAge();
+		Student[] array = {s};
+		assertEquals(expected, actualAge);
+		assertNotNull(s.avgAge(array));
+	}
+	}
+	
+	
